b20a3ae7ea1e21d271103d6fee1080aa0c10a53f
bszpryngiel <bartekszpryngiel@yahoo.pl>
2019-12-05 18:34:42 +0100

    Bootstrapping - initial client and server config.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        My init app
+    </div>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require( 'path' );
+
+module.exports = {
+   context: __dirname,
+   entry: './src/index.js',
+   output: {
+      path: path.resolve( __dirname, 'dist' ),
+      filename: 'main.js',
+      publicPath: '/',
+   },
+   devServer: {
+      historyApiFallback: true,
+      port: 3000
+   },
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: 'babel-loader',
+         },
+         {
+            test: /\.(png|j?g|svg|gif)?$/,
+            use: 'file-loader'
+         }
+      ]
+   },
+   resolve: {
+       alias: {
+           src: path.resolve(__dirname, 'src'),
+       },
+       extensions: ['.js', '.jsx'],
+   },
+   plugins: [
+      new Dotenv(),
+      new CleanWebpackPlugin(),
+      new HtmlWebPackPlugin({
+         template: path.resolve( __dirname, 'public/index.html' ),
+         filename: 'index.html'
+      })
+   ]
+};
